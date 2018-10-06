66d0ac4a583e305fd667f948184481e1619afca1
vdemy <vdemy.net@gmail.com>
2018-10-06 16:24:15 +0700

    Finish section 3.

---
+++ section-3-using-conditionals-and-rendering-lists/app4.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Laxa', age: 22, color: 'green'},
+            {name: 'Clarle', age: 18, color: 'blue'}
+        ]
+    }
+});
---
+++ section-3-using-conditionals-and-rendering-lists/exercise/app.js
@@ -0,0 +1,12 @@
+new Vue({
+	el:'#app', 
+  data: {
+  	show: true,
+    elements: ['Guitar', 'Books', 'Paper', 'Bag'],
+    movie: {title: 'Zootopia', author: 'Byron Howard', books: 3},
+    obj: {
+    	name: 'MyNumbers',
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+}); 
